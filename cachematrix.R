fd1ec4998fb4ccb4d52fa38d5910e7ea2cd77b91
Nan Wang@Home.MBP <Nan.Wang.28@gmail.com>
2014-08-19 18:12:03 +0200

    complete the first version.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## 'makeCacheMatrix(x)' creates a wrapper of a matrix with its inverse cached
+## returns a list of functions used to operate on the matrix and its inverse,
+## which is used by cacheSolve()
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## 'x': a numeric matrix, which is assumed to be invertible
+  
+  ## Return a list of functions:
+  ## 'set(new_x)': set the current matrix to a new matrix
+  ## 'get()': get the current matrix
+  ## 'set_inverse(new_inv)': set the cached matrix inverse to a new value
+  ## 'get_inverse()': get the current matrix
+  
+  inv_x <- NULL
+  set <- function(new_x) {
+    ## 'new_x': a new matrix
+    x <<- new_x
+    inv_x <<- NULL
+  }
+  get <- function() {
+    ## Return the current matrix
+    x
+  }
+  set_inverse <- function(new_inv) {
+    ## 'new_inv': a new matrix inverse
+    inv_x <<- new_inv
+  }
+  get_inverse <- function() {
+    ## Return the current matrix inverse
+    inv_x
+  }
+  list(set=set, get=get, 
+       set_inverse=set_inverse, get_inverse=get_inverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## 'cacheSolve()' calculate the inverse of the cached matrix.
+## If the inverse of the matrix has been cached, return the cached inverse,
+## else calculate the inverse matrix, save it to the cache and return it.
+cacheSolve <- function(cached_x, ...) {
+  ## 'cached_x': a list of functions that is returned by makeCacheMatrix
+  
+  ## Return a matrix that is the inverse of 'cached_x'
+  inv_x <- cached_x$get_inverse()
+  if(!is.null(inv_x)){
+    message("getting cached data")
+    return(inv_x)
+  }
+  data <- cached_x$get()
+  new_inv <- solve(data)
+  cached_x$set_inverse(new_inv)
+  new_inv
 }
---
+++ test_cachematrix.R
@@ -0,0 +1,47 @@
+## This is a test file for checking the basic functions of the implementations.
+setwd('.')
+source('cachematrix.R')
+## test code
+foo_mat <- matrix( 1:4, nrow=2, ncol=2)
+
+# test 1 makeCacheMatrix
+cached_mat <- makeCacheMatrix(foo_mat)
+## test 1.1: get
+cached_mat$get()
+### expected result
+# [,1] [,2]
+# [1,]    1    3
+# [2,]    2    4
+## test 1.2: set_inverse
+new_inv <- solve(foo_mat)
+cached_mat$set_inverse(new_inv)
+## test 1.3: get_inverse
+cached_mat$get_inverse()
+### expected result
+# [,1] [,2]
+# [1,]   -2  1.5
+# [2,]    1 -0.5
+## test 1.4: set
+foo_mat <- cached_mat$get_inverse()
+cached_mat$set(foo_mat)
+cached_mat$get()
+### expected result
+# [,1] [,2]
+# [1,]   -2  1.5
+# [2,]    1 -0.5
+cached_mat$get_inverse()
+### expected result
+# NULL
+
+# test 2 cacheSolve
+cacheSolve(cached_mat)
+### expected result
+# [,1] [,2]
+# [1,]    1    3
+# [2,]    2    4
+cacheSolve(cached_mat)
+### expected result
+# getting cached data
+# [,1] [,2]
+# [1,]    1    3
+# [2,]    2    4
